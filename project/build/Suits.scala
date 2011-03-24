2d02ca5a76abb2c3edcf6eae9f5590e6b7e14419
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2011-03-23 22:53:55 -0600

    Updated build files.

--- project/build/Suits.scala
+++ project/build/Suits.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class Suits(info: ProjectInfo) extends DefaultProject(info) {
+  override def managedStyle = ManagedStyle.Maven
   val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 }
 
